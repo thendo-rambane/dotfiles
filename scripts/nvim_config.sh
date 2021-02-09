bdaf2d506a2acd75aae40f125b817981388a64fb
FateXii <rambane.t.r@gmail.com>
2021-02-09 19:15:11 +0200

    Fix directory for virtual env installs

--- scripts/nvim_config.sh
+++ scripts/nvim_config.sh
@@ -8,17 +8,19 @@ echo "[Install virtualenvs]===================================="
 
 #setup vim python virtual envs
 virtualenv -p python2 ~/virtualenvs/pynvim
-cd ~/virtualenvs/nvimpy
+cd ~/virtualenvs/pynvim
 ./bin/pip install pynvim  
+cd ~
 
 virtualenv -p python3 ~/virtualenvs/pynvim3
-cd ~/virtualenvs/nvimpy3
+cd ~/virtualenvs/pynvim3
 ./bin/pip install pynvim  
-
+cd ~
 
 virtualenv -p python3 ~/virtualenvs/black
 cd ~/virtualenvs/black
 ./bin/pip install black  
+cd ~
 
 
 echo -e "\n"
