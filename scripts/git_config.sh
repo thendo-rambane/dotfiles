0d0d8194143e4366dfb952177e35fcbab1118cdf
FateXii <rambane.t.r@gmail.com>
2021-02-09 19:34:44 +0200

    ssh keygen check with which

--- scripts/git_config.sh
+++ scripts/git_config.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 echo -e "\n"
 echo "======================================[Create .gitconfig]"
-echo -e "\n"
 
 read -p "Enter git Email: " git_email
 read -p "Enter git Name: " git_name
 read -p "Enter git Username: " git_username
 
-git_cofig_file="~/dotfiles/.gitconfig"
-git_cofig_file_contents="[user]\n"
-git_cofig_file_contents=$git_cofig_file_contents"\temail = ${git_email}\n"
-git_cofig_file_contents=$git_cofig_file_contents"\tname = ${git_name}\n"
-git_cofig_file_contents=$git_cofig_file_contents"\n"
-git_cofig_file_contents="[credential]\n"
-git_cofig_file_contents=$git_cofig_file_contents"\tusername = ${git_username}\n"
-echo -e "${git_cofig_file_contents}" > $git_cofig_file
+git_config_file="~/dotfiles/.gitconfig"
+touch -p $git_config_file
+git_config_file_contents="[user]\n"
+git_config_file_contents=$git_config_file_contents"\temail = ${git_email}\n"
+git_config_file_contents=$git_config_file_contents"\tname = ${git_name}\n"
+git_config_file_contents=$git_config_file_contents"\n"
+git_config_file_contents="[credential]\n"
+git_config_file_contents=$git_config_file_contents"\tusername = ${git_username}\n"
+echo -e "${git_config_file_contents}" > $git_config_file
 
 echo -e "\n"
 echo "=========================================[Git ssh config]"
-echo -e "\n"
 
 pc_name=$(hostname)
-secrets_dir=$HOME"/.secrets/"
+secrets_dir=$HOME".secrets/"
 git_key_dir=$secrets_dir"/git_api_key"
 
 read -p "Enter git ssh key title(default ${pc_name}): " ssh_pub_key_title
@@ -35,6 +34,8 @@ if [ -z "$git_pub_api_key" ];
 then
   if [ ! -e "$git_key_dir" ];
   then
+    mkdir "$secrets_dir"
+    touch "$git_key_dir"
     cat /mnt/c/Users/ramba_enc4ybi/OneDrive/Documents/keys/git/api_key.txt > $git_key_dir
   fi
   git_pub_api_key="$(cat "$git_key_dir")"
@@ -43,31 +44,24 @@ fi
 
 echo -e "\n"
 echo "[openssh][Checking]======================================"
-echo -e "\n"
 
-if [ -z "$(ssh-keygen)"];
+if [ -z "$(which ssh-keygen)"];
 then
-echo -e "\n"
-echo "[openssh][Not found]====================================="
-echo -e "\n"
+  echo -e "\n"
+  echo "[openssh][Not found]====================================="
+
   sudo apt update
   sudo apt install openssh-client
-
-  #restart sshd (openssh daemon)
-
-
-  #systemctl restart sshd
-  /etc/init.d/sshd restart
 else
-echo -e "\n"
-echo "[openssh][Found]========================================="
-echo -e "\n"
+
+  echo -e "\n"
+  echo "[openssh][Found]========================================="
+
 fi
 
 
 echo -e "\n"
 echo "[openssh][Generate public ssh key]======================="
-echo -e "\n"
 
 if [ -z "$(cat ~/.ssh/id_rsa.pub)" ];
 then
@@ -77,7 +71,6 @@ fi
   
 echo -e "\n"
 echo "[openssh][Setting github ssh key]======================="
-echo -e "\n"
 
 curl -u "$git_username:$git_pub_api_key" \
   --data '{"title":"'"$ssh_pub_key_title"'","key":"'"$ssh_pub_key"'"}' \
@@ -86,7 +79,7 @@ curl -u "$git_username:$git_pub_api_key" \
 
 echo -e "\n"
 echo "===================================[Symlink .gitconfig]"
-echo -e "\n"
-ls -s "$git_cofig_file" ~/.gitconfig
+
+ls -s "$git_config_file" ~/.gitconfig
 
 
