428f49efc6b809576464d2735416020657a95df2
FateXii <rambane.t.r@gmail.com>
2021-02-10 18:12:27 +0200

    Use command -v to conditionally install nvm & node

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -1,8 +1,15 @@
 #!/user/bin
-curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 
-bash install_nvm.sh
+if [ -z "$(command -v nvm)" ];
+then
+  curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
+  bash install_nvm.sh
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
 
-source ~/.bashrc
-
-nvm install --lts
+if [ -z "$(command -v node)" ];
+then
+  nvm install --lts
+fi
