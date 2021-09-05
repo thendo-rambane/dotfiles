d5a1f11e590646d166fbbd2c3c62e0cd0cb27d8d
Thendo Rambane <rambane.t.r@gmail.com>
2021-09-05 17:49:35 +0200

    Add new laravel project script

--- bin/laravel.sh
+++ bin/laravel.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-if [ -z  "${1}" ];
+while getopts ":n:o:" arg; do
+  case $arg in
+    n) Name=$OPTARG;;
+    o) Options+=("$OPTARG");;
+  esac
+done
+
+function join { local d=${1-} f=${2-}; if shift 2; then printf %s "$f" "${@/#/$d}"; fi; }
+
+opts="mysql,mailhog"
+
+if [ ${#Options[@]} -gt 0 ] 
 then
-  app_name=${1}
-  echo -e "Please enter a laravel project name e,g:\n"
-  echo -e "\tlaravel {project_name}\e"
+  opts=$(printf ",%s" "${Options[@]}")
+  opts=${opts:1}
+fi
+
+echo -e "$opts"
+
+app_name=$(echo $Name | tr -d '\r\n')
+if [ -z  "$Name" ];
+then
+  echo -e "$app_name"
+  echo -e "Please enter a laravel project name e.g:\n"
+  echo -e "\tlaravel -n {project_name}\n"
 else
-  curl -s https://laravel.build/"$app_name" | bash
+  echo -e "$app_name"
+  curl -s "https://laravel.build/$app_name?with=$ops" | bash
 fi
 
